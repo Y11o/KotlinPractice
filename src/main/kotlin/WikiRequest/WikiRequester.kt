bb1244a0092fef1f970f67e23f5a1dfc93e2e3fb
Y11o <Y11o@users.noreply.github.com>
2022-09-29 22:53:14 +0300

    Browse

--- src/main/kotlin/WikiRequest/WikiRequester.kt
+++ src/main/kotlin/WikiRequest/WikiRequester.kt
@@ -1,6 +1,8 @@
 package WikiRequest
 
 import com.google.gson.Gson
+import java.awt.Desktop
+import java.net.URI
 
 class WikiRequester(
     private val request: String
@@ -16,6 +18,12 @@ class WikiRequester(
 
     fun browse(){
         println("Input the number of request, you want to open in browser...")
-
+        val userAnswer: Int = readln().toInt()
+        if (userAnswer > 0 && userAnswer <= searchResults.size){
+            Desktop.getDesktop().browse(URI("https://ru.wikipedia.org/w/index.php?curid=${searchResults[userAnswer - 1].pageId}"))
+        }else{
+            println("Incorrect input.")
+            browse()
+        }
     }
 }
